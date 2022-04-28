26dad416b59735d29a930c5926ee4495eeca3402
Ricky Kenny <10150884+rickykenny@users.noreply.github.com>
2022-04-28 18:04:20 +1000

    Add GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { UserContextProvider } from "../components/userContext";
 import { theme } from "../theme";
+import TagManager from "react-gtm-module";
+
+const TagManagerId = process.env.NEXT_PUBLIC_GTM_ID;
 
 function MyApp({ Component, pageProps }) {
+  // init in useEffect so it only runs client-side
+  useEffect(() => {
+    if (TagManagerId) {
+      TagManager.initialize({ id: TagManagerId });
+    }
+  }, [TagManagerId]);
+
   return (
     <ChakraProvider theme={theme}>
       <UserContextProvider>
